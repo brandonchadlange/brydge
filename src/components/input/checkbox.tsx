af1b63060b8e3fff7aad5cf373c143729809249c
Brandon <brandon.lange@aboutyou.com>
2023-01-31 13:54:30 +0100

    removed broken checkbox types

--- src/components/input/checkbox.tsx
+++ src/components/input/checkbox.tsx
@@ -9,15 +9,16 @@ type CustomCheckboxInputProps = {
 type CheckboxInputProps = CustomCheckboxInputProps;
 
 const MyCheckbox = ({ children, ...props }: { children: ReactNode; props: CheckboxInputProps }) => {
-  const [field, meta] = useField({ ...props, type: 'checkbox' });
+  // const [field, meta] = useField(props);
   return (
-    <>
-      <label htmlFor="acceptedTerms" className="flex flex-row justify-between mt-4">
-        <input className="mr-4 rounded-full" type="checkbox" {...field} {...props} />
-        {children}
-      </label>
-      {meta.touched && meta.error ? <div className="text-red-500 mb-8">{meta.error}</div> : null}
-    </>
+    // <>
+    //   <label htmlFor="acceptedTerms" className="flex flex-row justify-between mt-4">
+    //     <input className="mr-4 rounded-full" type="checkbox" {...field} {...props} />
+    //     {children}
+    //   </label>
+    //   {meta.touched && meta.error ? <div className="text-red-500 mb-8">{meta.error}</div> : null}
+    // </>
+    <></>
   );
 };
 
